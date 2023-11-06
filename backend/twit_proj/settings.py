a352481077462fac1f3fbcfea196fb73cf60b1cc
daniiar-mamaev <daniiar.mamaev@gmail.com>
2023-11-06 22:04:36 +0600

    fixed email

--- backend/twit_proj/settings.py
+++ backend/twit_proj/settings.py
@@ -149,3 +149,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = int(env("EMAIL_USE_TLS"))
+EMAIL_USE_SSL = int(env("EMAIL_USE_SSL"))
